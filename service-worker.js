cf1a111fbe6a81aa79ddd7fdb57701fea9d528de
Yaser Daqiq <yasir.dag@live.de>
2026-05-17 18:53:29 +0200

    Add fire PPL app icon

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "training-app-v1";
+const CACHE_NAME = "training-app-v2";
 
 const FILES_TO_CACHE = [
   "./",
@@ -6,6 +6,7 @@ const FILES_TO_CACHE = [
   "./style.css",
   "./script.js",
   "./manifest.json",
+  "./icons/icon.svg",
 ];
 
 self.addEventListener("install", (event) => {
